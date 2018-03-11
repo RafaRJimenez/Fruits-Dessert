e1287078f91721fb30179d6ae8cd725c272f68af
Rafa <rrl89@hotmail.com>
2018-03-11 20:54:32 +0100

    Setting addition and removing some fruits

--- javascript.js
+++ javascript.js
@@ -1,21 +1,46 @@
-var apple = "<div class='itemApple'> <div class='apple'></div> </div>";
+var apple = "<div class='item changeSize '> <div class='apple itemApple'></div> </div>";
+var orange = "<div class='item changeSize '> <div class='orange itemApple'></div> </div>";
 var matrizFruit = document.createElement('div');
-insertApple();
+var table;
+var checkApple=false;
 
-function insertApple(){
-	matrizFruit.innerHTML="<div class='matriz'><table>";
-	for (i=0;i<3;i++){
-		matrizFruit.innerHTML= matrizFruit.innerHTML + "<tr>";
-		for (z=0;z<3;z++){		
-			matrizFruit.innerHTML+="<td>" + apple + "</td>";
-			alert(matrizFruit.innerHTML);
+
+
+
+
+var pushApple=document.getElementsByClassName("pointerApple");
+pushApple[0].addEventListener('click', function(){insertApple("Apple");}, false);
+
+var pushOrange=document.getElementsByClassName("pointerOrange");
+pushOrange[0].addEventListener('click', function(){insertApple("Orange");}, false);
+
+
+function insertApple(fruit){
+	if (fruit==="Apple"){
+		if (checkApple==true){
+		var deleteElement = document.getElementsByClassName('matrizApple');
+		deleteElement[0].remove();
+		checkApple = false;
+		return;
 		}
-		matrizFruit.innerHTML+="</tr>";
+		var fruity=apple;
+		checkApple = true;
+	} else if (fruit==="Orange") {
+		var fruity = orange;
 	}
-	
+
+	table = "<div class='matriz"+fruit+"'><table>";
+	for (i=0;i<3;i++){
+		table += "<tr>";
+		for (z=0;z<3;z++){		
+			table+="<td>" + fruity + "</td>";
+		}
+		table+="</tr>";
+	}
+	table+="</table></div>";
+	matrizFruit.innerHTML += table;
+	document.getElementById('cake').appendChild(matrizFruit);
 }
 
-	matrizFruit.innerHTML+="</table></div>";
-	alert(matrizFruit.innerHTML);
+	
 
-document.getElementById('cake').appendChild(matrizFruit);
