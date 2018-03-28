60e15a40e14dbfc4730b300009fd0c8f8d730e10
Rafa <rrl89@hotmail.com>
2018-03-28 18:37:45 +0200

    Setting last changes and adding all ingredientes

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
-var apple = "<div class='item changeSize '> <div class='apple itemApple'></div> </div>";
-var orange = "<div class='item changeSize '> <div class='orange itemApple'></div> </div>";
+var apple = "<div class='item changeSize '> <div class='apple itemFruits'></div> </div>";
+var orange = "<div class='item changeSize '> <div class='orange itemFruits'></div> </div>";
+var banana = "<div class='item changeSize '> <div class='banana itemFruits'></div><div class='banana-line itemFruits'></div> </div>";
+var blueberry = "<div class='item changeSize '> <div class='blueberry itemFruits'></div> </div>";
+var cherry = "<div class='item changeSize '> <div class='cherry cherry1 itemFruits'></div><div class='cherry cherry2 itemFruits'></div> </div>";
 var matrizFruit = document.createElement('div');
 var table;
 var checkApple=false;
-
-
-
+var checkOrange = false;
+var checkBanana = false;
+var checkBlueberry = false;
+var checkCherry = false;
 
 
 var pushApple=document.getElementsByClassName("pointerApple");
-pushApple[0].addEventListener('click', function(){insertApple("Apple");}, false);
+pushApple[0].addEventListener('click', function(){insertFruit("Apple");}, false);
 
 var pushOrange=document.getElementsByClassName("pointerOrange");
-pushOrange[0].addEventListener('click', function(){insertApple("Orange");}, false);
+pushOrange[0].addEventListener('click', function(){insertFruit("Orange");}, false);
+
+var pushBanana=document.getElementsByClassName("pointerBanana");
+pushBanana[0].addEventListener('click', function(){insertFruit("Banana");}, false);
+
+var pushBlueberry=document.getElementsByClassName("pointerBlueberry");
+pushBlueberry[0].addEventListener('click', function(){insertFruit("Blueberry");}, false);
 
 
-function insertApple(fruit){
+var pushCherry=document.getElementsByClassName("pointerCherry");
+pushCherry[0].addEventListener('click', function(){insertFruit("Cherry");}, false);
+
+
+function insertFruit(fruit){
 	if (fruit==="Apple"){
-		if (checkApple==true){
+		while (checkApple==true){
 		var deleteElement = document.getElementsByClassName('matrizApple');
 		deleteElement[0].remove();
 		checkApple = false;
@@ -25,10 +39,72 @@ function insertApple(fruit){
 		}
 		var fruity=apple;
 		checkApple = true;
+		settingFruits(fruit,fruity);
 	} else if (fruit==="Orange") {
+		while (checkOrange==true){
+		var deleteElement = document.getElementsByClassName('matrizOrange');
+		deleteElement[0].remove();
+		checkOrange = false;
+		return;
+		}
 		var fruity = orange;
+		checkOrange = true;
+		settingFruits(fruit,fruity);
+	}
+	else if (fruit==="Banana") {
+		while (checkBanana==true){
+		var deleteElement = document.getElementsByClassName('matrizBanana');
+		deleteElement[0].remove();
+		checkBanana = false;
+		return;
+		}
+		var fruity = banana;
+		checkBanana = true;
+		settingFruits(fruit,fruity);
+	}
+	else if (fruit==="Blueberry") {
+		while (checkBlueberry==true){
+		var deleteElement = document.getElementsByClassName('matrizBlueberry');
+		deleteElement[0].remove();
+		checkBlueberry = false;
+		return;
+		}
+		var fruity = blueberry;
+		checkBlueberry = true;
+		settingFruits(fruit,fruity);
 	}
 
+	else if (fruit==="Cherry") {
+		while (checkCherry==true){
+		var deleteElement = document.getElementsByClassName('matrizCherry');
+		deleteElement[0].remove();
+		checkCherry = false;
+		return;
+		}
+		var fruity = cherry;
+		checkCherry = true;
+		settingFruits(fruit,fruity);
+	}
+	/*
+	table = "<div class='matriz"+fruit+"'><table>";
+	for (i=0;i<3;i++){
+		table += "<tr>";
+		for (z=0;z<3;z++){		
+			table+="<td>" + fruity + "</td>";
+		}
+		table+="</tr>";
+	}
+	table+="</table></div>";
+	matrizFruit.innerHTML += table;
+	document.getElementById('cake').appendChild(matrizFruit);
+	*/
+}
+
+
+// fruit --> Matching the string of the fruit to a class name to set the single matrix
+// fruity --> Matching the single divs of the fruit to each td of the table
+
+function settingFruits(fruit,fruity){
 	table = "<div class='matriz"+fruit+"'><table>";
 	for (i=0;i<3;i++){
 		table += "<tr>";
